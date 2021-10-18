7c53930f264abb5ffe3935ca84d204c6b56898dd
Aidan <aidanlowson@gmail.com>
2021-10-18 23:23:09 +0100

    Build #1

---
+++ public/component---src-pages-blogs-js-a4c3934bf3d89b24262b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdouble_a_team=self.webpackChunkdouble_a_team||[]).push([[296],{6188:function(e,t,a){a.r(t);var l=a(7294),n=a(5444),u=a(8209),c=a(3751);t.default=function(){return l.createElement(u.Z,null,l.createElement(c.Z,{title:"Blogs"}),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-blogs-js-a4c3934bf3d89b24262b.js.map
