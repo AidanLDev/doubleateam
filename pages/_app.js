af86f2b3c234caa90a78a7ccc3814b66b4b5c69f
Aidan <aidan.lowson@contactpartners.com>
2022-03-19 14:47:50 +0000

    Adding themeing

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+const theme = extendTheme({ colors });
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
