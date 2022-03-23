82a5917d601993e6519dd36c62dc757797805550
Aidan <aidan.lowson@contactpartners.com>
2022-03-23 14:25:43 +0000

    Adding fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,20 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import "@fontsource/aldrich";
+import "@fontsource/open-sans";
+
 
 // 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
+const theme = extendTheme({
+  fonts: {
+    heading: 'Aldrich, sans-serif',
+    body: 'Open Sans, sans-serif'
+  },
+  colors: {
     main: '#F6F4F1',
     primary: '#FF0000',
     secondary: '#48AAF4',
-  },
-};
-
-const theme = extendTheme({ colors });
+  }
+});
 
 // 3. Pass the `theme` prop to the `ChakraProvider`
 
