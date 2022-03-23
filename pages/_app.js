0c913f50d99069e1579e88bdcf7de4865e3c44d1
Aidan <aidan.lowson@contactpartners.com>
2022-03-23 12:31:43 +0000

    Updating themeing

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,9 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
   brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+    main: '#F6F4F1',
+    primary: '#FF0000',
+    secondary: '#48AAF4',
   },
 };
 
