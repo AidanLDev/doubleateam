2a22c688b962e3593e3d0a75d7be025469fe71e1
Aidan <aidan.lowson@contactpartners.com>
2022-06-13 18:49:25 +0100

    Adding search

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { Heading, List, ListItem } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Flex, Heading, List, ListItem, Input, Text } from '@chakra-ui/react';
 import { getBlogPostPaths } from '../../lib/posts';
 import BackHomeLink from '../../components/BackHomeLink';
 
@@ -14,22 +15,39 @@ export async function getStaticProps() {
 }
 
 export default function Posts({ sortedPaths }) {
+  const [filterValue, setFilterValue] = useState('');
+
+  const handleInputChange = e => {
+    setFilterValue(e.target.value);
+  };
+
   return (
     <>
       <Head>
         <title>Double A Team | Blogs</title>
       </Head>
-      <Heading color='primary'>All Blogs</Heading>
+      <Flex>
+      <Heading color='primary' width="50%">
+        <Link href={'/'}>Double A Team</Link>
+      </Heading>
+      <Input variant='outline' placeholder='Search' width="50%" value={filterValue} onChange={handleInputChange}/>
+      </Flex>
+      <Text>
+        Spreading what we know, with those who want to know with as much clarity as possible.
+      </Text>
       <List spacing={3}>
         {sortedPaths.map((post, idx) => {
           if (post !== 'index') {
-            return (
-              <ListItem key={`${post}__${idx}`}>
-                <Link href={`/posts/${post}`} passHref>
-                  <a>{post}</a>
-                </Link>
-              </ListItem>
-            );
+            const neatenedPost = post.replaceAll('-', ' ');
+            if (filterValue === '' || RegExp(filterValue, 'i').test(neatenedPost)) {
+              return (
+                <ListItem key={`${post}__${idx}`}>
+                  <Link href={`/posts/${post}`} passHref>
+                    <a>{neatenedPost}</a>
+                  </Link>
+                </ListItem>
+              );
+            }
           }
         })}
       </List>
