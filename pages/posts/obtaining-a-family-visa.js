fdc74c74280814a0bbd425fe55090397cde3b770
Aidan <aidansoftwaredev@gmail.com>
2022-07-01 18:32:45 +0100

    blog WIP

--- pages/posts/obtaining-a-family-visa.js
+++ pages/posts/obtaining-a-family-visa.js
@@ -9,18 +9,81 @@ export default function HowToPassIelts() {
         <title>Double A Team | How to pass IELTs</title>
       </Head>
       <Heading className='blogHeading'>Getting a UK Family Visa</Heading>
+      <Heading size='md'>Expenses</Heading>
       <Text>
-        Minim occaecat sunt voluptate exercitation sunt do. Enim esse dolor
-        laborum incididunt magna occaecat. Aute esse sit magna fugiat minim non
-        incididunt deserunt pariatur mollit exercitation non incididunt.
+        For the family visa it’s quite expensive. It cost us £1,682 for the
+        application and an extra £2,047 for the IHS (health insurance surge)
+        payment that’s required as part of the application. We needed to get it
+        right, to live our lives together and build our future.
       </Text>
+      <Heading size='md'>Visa Consultant</Heading>
       <Text>
-        Sunt et esse sint voluptate est et eiusmod. Commodo ad pariatur commodo
-        aliqua ea. Occaecat occaecat cillum labore nisi excepteur nostrud minim
-        aliqua. Esse ipsum et magna anim reprehenderit aliquip nulla pariatur.
-        Pariatur est tempor non consequat est eiusmod quis exercitation
-        consectetur velit aute consectetur commodo.
+        That’s why we hired a visa consultant from{' '}
+        <a href='https://www.i-vars.com/'>I-vars</a>. Their service was
+        unbelievably helpful, they start with an initial call, going through the
+        process itself and they get to understand you and your situation so they
+        can know how to best help. Once signed up they send a pack with all the
+        information about the visa application process and documents required to
+        get the approval we all need. They will review any documents and give
+        guidance throughout the process.
       </Text>
     </BlogLayout>
   );
 }
+
+/*
+UK family visa guide
+Expenses
+For the family visa it’s quite expensive. It cost us £1,682 for the application and an extra £2,047 for the IHS (health insurance surge) payment that’s required as part of the application. We needed to get it right, to live our lives together and build our future.
+Visa Consultant
+That’s why we hired a visa consultant from I-vars: https://www.i-vars.com/. Their service was unbelievably helpful, they start with an initial call, going through the process itself and they get to understand you and your situation so they can know how to best help. Once signed up they send a pack with all the information about the visa application process and documents required to get the approval we all need. They will review any documents and give guidance throughout the process.
+The application
+On to the application itself, it’s all done online through the gov website: https://www.gov.uk/uk-family-visa/partner-spouse
+The majority of questions are self-explanatory. A few stand out ones that could do with some further clarification:
+•	When did you first meet?
+o	As we met online, we weren’t really sure whether to put the date we first talked online or the first time we physically met each other in person for the first time.
+We were advised to put down the date we first talked to each online
+•	Have you been declined entry or had a visa application declined before?
+o	Sadly, we had failed to obtain a visitor visa in the past, so we had to put down yes and explain a little about why. To get the wording 100% we reached out and were advised to put the following (this will be specific to our situation as the reason the visitor visa was declined was due to UKVI thinking ties to the original country weren’t strong enough and by the end of the visa, they thought she wouldn’t leave the UK)
+I applied for a visit visa to the UK so I could spend some time with my husband (at the time fiancé) Aidan. I really wanted to spend some quality time with him and meet his family, but sadly the visa application was declined The ECO was not completely satisfied I would return to Indonesia at the end of the trip
+The application is pretty lengthy but most of the questions you just need to input data about yourself so they’re not too difficult.
+Once you’ve paid and submitted your application, you will have to book an appointment to provide Biometric data. Once this appointment has been booked you can submit your supporting evidence to the visa centre.
+Supporting Documents
+First off, it helps to know what you’re trying to prove with the documents;
+•	Your partner or family member is one of the following:
+o	Has a British Citizenship
+o	Settlement or EU pre-settled status in the UK
+o	Asylum or humanitarian protection in the UK
+•	You’re over 18
+•	You’re in a marriage that is recognised in the UK
+•	You intend to continue to living together in the UK after you apply
+•	You and your family will have an adequate place to live in the UK
+•	You have a good knowledge of English (either a degree taught in English validated by ECCTIS or a secure test at A1 CERF minimum
+•	You meet the financial requires (your partners salary must be over £18,600, which your partner has to of earned for the last 6 months at least)
+•	You’re free from TB (if you live in a country that requires this)
+In my opinion, this is the most difficult part, the actual documents checklist is relatively short. However if you want a really solid application (which we all do, especially considering how pricy it is to even apply), then you’re going to want to submit a lot of supporting documents.
+Conditional Docs
+•	TB Test
+o	Depending on where you live, you may need to take a TB test (click here to see the list of countries https://www.gov.uk/tb-test-visa/countries-where-you-need-a-tb-test-to-enter-the-uk )
+o	If you do need one, click on your country from the above link, and a list of approved test centres/doctors will be shown, along with their contact details which is super handy
+•	SELT (Secure English Language Test)
+o	If you have a degree that was taught in English, you may not need to take this test
+o	However if you do, more information and a list of approved test centres can be found here: https://www.gov.uk/guidance/prove-your-english-language-abilities-with-a-secure-english-language-test-selt
+o	We have some specific details about passing the IELTs A1 English test here
+General documents
+•	Current and any old passports
+•	Copy of your partners passport
+•	Proof of your relationship (including your marriage certificate and evidence relating to time you have spent together as a couple)
+•	Cover letters from you and your partner, together with supporting statements, including photographic evidence, from family, friends, employers etc. As you deem necessary to prove your relationship is genuine
+•	Sponsorship declaration from your partner/family member
+•	Proof that any earlier relationships for either of you were over before you married (such as an affidavit or affirmation)
+Financial and accommodation
+•	A letter from your partners employers confirming their role, salary and tenure
+•	Bank statements going back 6 months
+•	Payslips going back 6 months
+•	Evidence for your UK accommodation such as; estate agent’s details, tenancy agreements, a letter from the landlord if you’re renting
+Now you’ve got all that, all that’s left is the waiting game.
+Usually, the visa will take less than 3 months to process, however with the pandemic there’s a bit of a back log and it could take up to 4 months or so.
+All the best of luck to you and we wish you and your partner a happy life together
+AL & AR
+*/
