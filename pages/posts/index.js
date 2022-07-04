a93037a310cc753829c0495c32419bc7fa04c481
AidanLDev <dev@aidanlowson.com>
2022-07-04 23:40:43 +0100

    lint

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -62,6 +62,7 @@ export default function Posts({ blogs }) {
   };
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     flashingTextWidth = innerWidth;
     setTimeout(function () {
       timeoutFunction();
--- pages/posts/obtaining-a-family-visa.js
+++ pages/posts/obtaining-a-family-visa.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from 'next/head';
 import Link from 'next/link';
 import { Heading, Text } from '@chakra-ui/react';
