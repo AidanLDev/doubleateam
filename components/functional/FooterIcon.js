0174026d2738d59334fcde2109d47ac4d7ebe1b0
AidanLDev <dev@aidanlowson.com>
2022-07-17 23:41:14 +0100

    Themeing update

--- components/functional/FooterIcon.js
+++ components/functional/FooterIcon.js
@@ -4,7 +4,12 @@ export default function FooterIcon({ icon, link, hoverColour }) {
   return (
     <>
       <a href={link} target='_blank' rel='noreferrer'>
-        <Icon as={icon} cursor='pointer' _hover={{ color: hoverColour }} />
+        <Icon
+          as={icon}
+          cursor='pointer'
+          _hover={{ color: hoverColour }}
+          color='#0070f3' //  Link colour
+        />
       </a>
     </>
   );
