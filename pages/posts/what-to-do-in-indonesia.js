145c71740126352ed3405cadac4663516dd2d6d1
Aidan <dev@aidanlowson.com>
2022-07-23 21:11:54 +0100

    Starting blog

--- pages/posts/what-to-do-in-indonesia.js
+++ pages/posts/what-to-do-in-indonesia.js
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react';
+import { Text, Heading } from '@chakra-ui/react';
 import BlogLayout from '../../components/blogLayout/BlogLayout';
 import BlogTitle from '../../components/BlogTitle';
 
@@ -12,16 +12,26 @@ export default function HowToPassIelts() {
     >
       <BlogTitle text={blogTitle} />
       <Text>
-        Minim occaecat sunt voluptate exercitation sunt do. Enim esse dolor
-        laborum incididunt magna occaecat. Aute esse sit magna fugiat minim non
-        incididunt deserunt pariatur mollit exercitation non incididunt.
+        This is going to be a quick summary of a few places we visited and a
+        little bit of insight into them to help you plan your Indonesian
+        getaway. We will go into more depth on places we think deserve more
+        attention in seperate blogs but for a nice overview, here is our
+        Indonesian travel guide.
+      </Text>
+      <Heading size='md'>Bandung</Heading>
+      <Text>
+        My wife could go on for days about what Bandung is like as a city in
+        general as she has spent most of her life living there! From a western
+        perspective, Bandung is a breath of freshair, it's the first city I
+        visited after Jakarta and the climate is so much fresher. The air feels
+        clean as it's surrounded by mountains/volcanos and the temprature is
+        cooler as a result. If you're into hiking there's plenty around Bandung
+        to get stuck into. The last couple of times we went, it was the rainy
+        season so we gave that a miss but here are some reccomendations from
+        Arni... todo({/* TODO: add photos and a few hiking places from Arni */})
       </Text>
       <Text>
-        Sunt et esse sint voluptate est et eiusmod. Commodo ad pariatur commodo
-        aliqua ea. Occaecat occaecat cillum labore nisi excepteur nostrud minim
-        aliqua. Esse ipsum et magna anim reprehenderit aliquip nulla pariatur.
-        Pariatur est tempor non consequat est eiusmod quis exercitation
-        consectetur velit aute consectetur commodo.
+        <b>Kawah Ratu</b>
       </Text>
     </BlogLayout>
   );
