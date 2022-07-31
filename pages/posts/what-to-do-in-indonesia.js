67c33325d82752bd257278551e0a46d6b07ec289
Aidan <dev@aidanlowson.com>
2022-07-31 15:36:34 +0100

    Finishing up Indo travel guide

--- pages/posts/what-to-do-in-indonesia.js
+++ pages/posts/what-to-do-in-indonesia.js
@@ -61,13 +61,13 @@ export default function HowToPassIelts() {
         and where we'd reccomed based on our experiance. A few places to note
         are:
       </Text>
-      <strong>Nice resturant</strong>
+      <strong>Candhari Heaven restaurant</strong>
       <Image
-        alt='enter res name here'
+        alt='Candhari Heaven'
         src='/images/blog/whatToDoInIndonesia/Jogja/niceResturant.jpg'
       />
       <Text>
-        To eat some delicious food with the most stunning backdrop and a few
+        To eat some delicious food with the most stunning backdrop and multiple
         places for memorable photos. We couldn't recommend this place enough.
         They sell a selection of refreshing drinks too so even after you're done
         eating, you can just relax with a few drinks and soak up that unique
@@ -75,7 +75,7 @@ export default function HowToPassIelts() {
       </Text>
       <strong>Malioboro</strong>
       <Image
-        alt='enter res name here'
+        alt='Malioboro street'
         src='/images/blog/whatToDoInIndonesia/Jogja/malioboro.jpg'
       />
       <Text>
@@ -85,7 +85,7 @@ export default function HowToPassIelts() {
       </Text>
       <strong>Borobudur</strong>
       <Image
-        alt='enter res name here'
+        alt='Borobudur'
         src='/images/blog/whatToDoInIndonesia/Jogja/boroboudor.jpg'
       />
       <Text>
@@ -97,12 +97,46 @@ export default function HowToPassIelts() {
       <Text>
         During our journey through Java, we made our way east to Malang, this
         city feels so refreshing, surrounded by the most beautiful mountainous
-        scenery. We had a tour with "ENTER BROMO TOUR COMPANY HERE" and we
-        couldn't reccomed them enough. They were very reasonable in price
+        scenery. We had a tour with{' '}
+        <SimpleLink link='https://www.explorebromo.com/'>
+          Explore Bromo
+        </SimpleLink>{' '}
+        and we were so chuffed we chose them. They were very reasonable in price
         compared to some other and it only cost a little extra to include a
         camera man who showed us all the best spots, gave plenty of background
         and of course took some stunning photos:
       </Text>
+      <Image
+        alt='Bromo tour pic'
+        src='/images/blog/whatToDoInIndonesia/Malang/DSC01948.JPG'
+      />
+      <Image
+        alt='Bromo tour pic'
+        src='/images/blog/whatToDoInIndonesia/Malang/DSC01982.JPG'
+      />
+      <Image
+        alt='Bromo tour pic'
+        src='/images/blog/whatToDoInIndonesia/Malang/DSC01999.JPG'
+      />
+      <Image
+        alt='Bromo tour pic'
+        src='/images/blog/whatToDoInIndonesia/Malang/DSC02006.JPG'
+      />
+      <Image
+        alt='Bromo tour pic'
+        src='/images/blog/whatToDoInIndonesia/Malang/DSC02153.JPG'
+      />
+      <Image
+        alt='Bromo tour pic'
+        src='/images/blog/whatToDoInIndonesia/Malang/20220603_012448778_iOS.jpg'
+      />
+      <Text>
+        With your eyes blessed with this stunning Indonesian nature, we hope you
+        too can enjoy and explore this enchanting country. The sheer size of it
+        means you can visit this country time and time again and have a
+        different experiance every time.
+      </Text>
+      <Text>We hope you enjoy your time and have safe travels ✈️🌍☀️🏔️</Text>
     </BlogLayout>
   );
 }
