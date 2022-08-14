bc637b27093288c018bf390a4a5d5126b773cbad
Aidan <dev@aidanlowson.com>
2022-08-14 16:56:25 +0100

    Sitemap stuff

---
+++ next-sitemap-config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://doubleateam.co.uk',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
+
+// export default config;
