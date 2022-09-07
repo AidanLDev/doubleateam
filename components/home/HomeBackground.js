9027ea2c67695d95fced27e18b834fe28f788683
AidanLDev <dev@aidanlowson.com>
2022-09-07 22:51:38 +0100

    Using next img comp [ci skip]

--- components/home/HomeBackground.js
+++ components/home/HomeBackground.js
@@ -1,18 +1,24 @@
 import { Box } from '@chakra-ui/react';
+import Image from 'next/image';
+import useIsMobile from '../../hooks/useIsMobile';
 
 export default function HomeBackground() {
+  const isMobile = useIsMobile();
   return (
     <Box
-      bg='main'
-      bgImage='url(/images/IstanbulHagiaSopia.avif)'
-      backgroundPosition='center'
-      backgroundRepeat='no-repeat'
-      h='100vh'
-      w='100%'
-      pos='absolute'
-      zIndex='10'
-      backgroundSize='cover'
+      position='absolute'
+      width='100%'
+      height='100vh'
       borderBottom='2px solid red'
-    />
+    >
+      <Image
+        src={`/images/IstanbulHagiaSopia${isMobile ? 'Mobile' : ''}.avif`}
+        alt='Hagia Sopia home image'
+        layout='fill'
+        width='100%'
+        height='100%'
+        priority
+      />
+    </Box>
   );
 }
