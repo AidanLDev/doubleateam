cd423ed62f2d374f411df01c6eaf644bb5a27e00
Aidan <aidan.lowson@davies-group.com>
2022-10-10 14:02:36 +0100

    Active link styles

--- components/Nav/Links.js
+++ components/Nav/Links.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import useIsMobile from '../../hooks/useIsMobile';
 import { useTranslation } from 'react-i18next';
+import Styles from './styles.module.scss';
+import { useRouter } from 'next/router';
 
 const transPath = 'topBar.';
 
@@ -14,22 +16,47 @@ export async function getStaticProps() {
 }
 
 export default function Links({ isHome }) {
+  const router = useRouter();
   const { t } = useTranslation();
   const isMobile = useIsMobile();
   return (
     <>
       <Link href='/posts'>
-        <a>{t(`${transPath}allBlogs`)}</a>
+        <a
+          className={
+            router.pathname === '/posts' ? Styles.active : ''
+          }
+        >
+          {t(`${transPath}allBlogs`)}
+        </a>
       </Link>
       {!isMobile && !isHome && <span>|</span>}
       <Link href='/about-us' passHref>
-        <a>{t(`${transPath}aboutUs`)}</a>
+        <a
+          className={
+            router.pathname === '/about-us' ? Styles.active : ''
+          }
+        >
+          {t(`${transPath}aboutUs`)}
+        </a>
       </Link>
       <Link href='/contact-us' passHref>
-        <a>{t(`${transPath}contactUs`)}</a>
+        <a
+          className={
+            router.pathname === '/contact-us' ? Styles.active : ''
+          }
+        >
+          {t(`${transPath}contactUs`)}
+        </a>
       </Link>
       <Link href='/privacy-policy' passHref>
-        <a>{t(`${transPath}privacyPolicy`)}</a>
+        <a
+          className={
+            router.pathname === '/privacy-policy' ? Styles.active : ''
+          }
+        >
+          {t(`${transPath}privacyPolicy`)}
+        </a>
       </Link>
     </>
   );
