4763e1f7d26487093b205c5499440ec7af5e41a2
Aidan Lowson <aidan.lowson@davies-group.com>
2023-03-12 00:27:48 +0000

    Adding astro [ci skip]

---
+++ astro-blog/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Double A Team';
+export const SITE_DESCRIPTION = 'Sharing what we know with those who want to know';
