c644284a39e8f0d9b857efa50618e813c004e9db
Aidan Lowson <aidan.lowson@davies-group.com>
2023-03-12 09:36:24 +0000

    Adding config [ci skip]

--- astro-blog-template/astro.config.mjs
+++ astro-blog-template/astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()],
+  site: 'https://doubleateam.co.uk',
+  integrations: [compress(), mdx(), tailwind(), sitemap()],
 })
