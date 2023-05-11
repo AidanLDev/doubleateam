3c29a9955e49599ba91ae1b05868a26e4eba6571
Aidan <aidan.lowson@contactpartners.com>
2023-05-11 16:48:08 +0100

    Swapping -> blog.aidanlowson

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://doubleateam.co.uk',
+  site: 'https://blog.aidanlowson.com',
   integrations: [compress(), mdx(), tailwind(), sitemap()],
 })
