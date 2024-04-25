46e1e802888141655cf1c2da2b5fca1c53807385
AidanLDev <dev@aidanlowson.com>
2024-04-25 16:53:29 +0100

    Compnents are all styled up!

--- src/components/posts/PostCard.jsx
+++ src/components/posts/PostCard.jsx
@@ -5,21 +5,21 @@ export default function PostCard({slug, title, pubDate, heroImage, description, 
   return (
     <div className={Styles.cardContainer}>
       <div className={Styles.cardImage}>
-        <img src={heroImage} alt={title} />
+        <img src={heroImage} alt={title} height='100%' width='100%' />
       </div>
       <div className={Styles.cardContent}>
-      <h3>
-        <a href={`/posts/${slug}`}>{title}</a>
-      </h3>
-      <p>
-        {description}
-      </p>
-      {tags && tags.length > 0 && (
-        tags.map(tag => <Tag tag={tag}/>)
-      )}
-      <small>
-        {pubDate}
-      </small>
+        <h3>
+          <a href={`/posts/${slug}`}>{title}</a>
+        </h3>
+        <p>
+          {description}
+        </p>
+        {tags && tags.length > 0 && <div className={Styles.tagContainer}>
+          {tags.map(tag => <Tag tag={tag}/>)}
+        </div>}
+        <small>
+          {pubDate}
+        </small>
       </div>
     </div>
   )
--- src/components/posts/Tag.jsx
+++ src/components/posts/Tag.jsx
@@ -2,7 +2,7 @@ import Styles from './styles.module.scss'
 
 export default function Tag({tag}) {
   return (
-    <div className={Styles.tagContainer}>
+    <div className={Styles.tag}>
       <span>{tag}</span>
     </div>
   )
