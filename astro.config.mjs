a2bcfaf554c87acb1777e285f062d2e74657476c
AidanLDev <dev@aidanlowson.com>
2024-04-26 09:27:35 +0100

    Fixing up tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,27 @@
-import { defineConfig } from 'astro/config';
-import icon from 'astro-icon';
-import compress from 'astro-compress';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
+import compress from 'astro-compress'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.aidanlowson.com',
-  integrations: [compress(), mdx(), tailwind(), sitemap(), icon(), preact()]
-});
+  integrations: [
+    compress(),
+    mdx(),
+    tailwind(),
+    sitemap(),
+    icon(),
+    preact(),
+    partytown({
+      config: {
+        forward: ['datalayer.push'],
+      },
+    }),
+  ],
+})
